d27bc48d420c621fa274df7d881cee7cf6a109de
J K <kj688895@gmail.com>
2020-09-16 17:30:18 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,10 +6,9 @@ def create_an_array
 candy = ["Skittles", "Snickers", "Kit Kat", "Gummy Bears"]
 end
 
-def add_element_to_end_of_array(candy_array, "Twix")
-candy_array = ["Skittles", "Snickers", "Kit Kat", "Gummy Bears"]
-candy_array << "Twix"
-puts candy_array.inspect
+def add_element_to_end_of_array(candy, "Twix")
+candy << "Twix"
+puts candy.inspect
 end
 
 def add_element_to_start_of_array(array, element)
